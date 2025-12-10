e68c3a7e63e87c417e97ac73a1512ebdb401dece
José Javier Blanco Rivero <javierweiss@gmail.com>
2025-12-10 13:21:59 +0000

    Initial commit

---
+++ dev/user.clj
@@ -0,0 +1,10 @@
+(user
+ (:require [portal.api :as p]
+           [com.brunobonacci.mulog :as μ]))
+
+(def p (p/open {:launcher :vs-code})) 
+
+(add-tap #'p/submit)
+
+(μ/start-publisher! {:type :console
+                     :pretty? true})
---
+++ src/mapacama/core.clj
@@ -0,0 +1,1 @@
+(ns mapacama.core)
